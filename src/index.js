e83a3012ff77d9266796331695fe66173ed3cfe3
Vikas Pathak <123vikas.vp79@gmail.com>
2023-03-27 14:16:06 +0530

    profile,landing page, make responsive changement

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
-
+import './index.css'
 import { AppProvider } from "./utils/context";
 
 
